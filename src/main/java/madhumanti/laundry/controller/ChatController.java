a69cf3da06973f88e1f4a9bd0dfdb48bed589204
Madhumanti <madhu.nb25@gmail.com>
2020-04-24 23:17:08 +0530

    text run

--- src/main/java/madhumanti/laundry/controller/ChatController.java
+++ src/main/java/madhumanti/laundry/controller/ChatController.java
@@ -6,6 +6,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Request;
+
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
@@ -21,11 +24,14 @@ public class ChatController {
 	@POST 
     @Path("/bot")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String chatbot(String incoming_msg) {
-		//ReceiveSMS s=new ReceiveSMS();
+    @Produces(MediaType.APPLICATION_XML)
+    public String chatbot(MultivaluedMap<String,String> request) {
+	    String incoming_message =request.get("Body").get(0).toString();
+	    System.out.println("request "+incoming_message);
+		//request=request.split("&")[4];
+		//String incoming_message=request.split("=")[1];
 		Body body = new Body
-                .Builder("The Robots are coming! Head for the hills!")
+                .Builder(incoming_message)
                 .build();
         Message sms = new Message
                 .Builder()
@@ -35,6 +41,7 @@ public class ChatController {
                 .Builder()
                 .message(sms)
                 .build();
+        System.out.println("message "+twiml.toXml());
         return twiml.toXml();
     }
 }
