2aaee39287ad30bc2a713ceab6d2ef33514592c4
Madhumanti <madhu.nb25@gmail.com>
2020-04-24 22:13:45 +0530

    media type

--- src/main/java/madhumanti/laundry/controller/ChatController.java
+++ src/main/java/madhumanti/laundry/controller/ChatController.java
@@ -6,9 +6,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-
-
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Body;
+import com.twilio.twiml.messaging.Message;
 
 @Path("/api")
 public class ChatController {
@@ -20,10 +20,21 @@ public class ChatController {
 	
 	@POST 
     @Path("/bot")
-    @Consumes(MediaType.APPLICATION_XML)
-    @Produces(MediaType.APPLICATION_XML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public String chatbot(String incoming_msg) {
-		
-        return null;
+		//ReceiveSMS s=new ReceiveSMS();
+		Body body = new Body
+                .Builder("The Robots are coming! Head for the hills!")
+                .build();
+        Message sms = new Message
+                .Builder()
+                .body(body)
+                .build();
+        MessagingResponse twiml = new MessagingResponse
+                .Builder()
+                .message(sms)
+                .build();
+        return twiml.toXml();
     }
 }
